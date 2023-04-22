942d9bb80d456e73e80e03214cc973977bd76be4
AbdalrahmanM <abdulrahman.al-samaraie@ogr.sakarya.edu.tr>
2023-04-22 20:13:15 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(array,driverName){
+    return array.filter(function(element){
+        return driverName.toUpperCase()==element.toUpperCase()
+    })
+}
+
+function fuzzyMatch(array,driverName){
+    return array.filter((element)=>{
+        return element.startsWith(driverName)
+    })
+}
+
+function matchName(array,driverName){
+    return array.filter((element)=>{
+        return element.name==driverName
+    })
+}
